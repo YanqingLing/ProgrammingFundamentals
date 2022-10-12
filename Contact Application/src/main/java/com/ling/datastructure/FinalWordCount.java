44eeacd40fab2a8556392e4409e6c8b9ccc3100a
Yanqing Ling <lingyanqing018@gmail.com>
2022-10-11 17:14:39 -0700

    Updated Version Of Word Count

--- Contact Application/src/main/java/com/ling/datastructure/FinalWordCount.java
+++ Contact Application/src/main/java/com/ling/datastructure/FinalWordCount.java
@@ -1,8 +1,12 @@
 package com.ling.datastructure;
 
-import java.util.ArrayList;
+import com.sun.source.tree.Tree;
+
+import java.util.*;
 
 public class FinalWordCount {
+    static HashMap<String, Integer> wordsHashMap = new HashMap<String, Integer>();
+
     public static void main(String[] args) {
         ArrayList<String> lines = new ArrayList<String>();
         lines.add("Nothing is so necessary for a young man as the company of intelligent women.");
@@ -13,5 +17,61 @@ public class FinalWordCount {
         lines.add("Well, what makes you go to war?");
 
         WordCount countWords = new WordCount();
+        ArrayList<String> allWords = new ArrayList<>();
+
+        for(String line : lines) {
+            String[] words = line.split("\\W+");
+            for(String word : words) {
+                allWords.add(word);
+            }
+        }
+
+        //Print all the words found alphabetically
+        TreeSet<String> sorted = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+        sorted.addAll(allWords);
+        System.out.println(sorted);
+
+        //Print the first and last word
+        System.out.println(allWords.get(0));
+        System.out.println(allWords.get(allWords.size()-1));
+
+        //Print all the unique words ignoring case - don’t sort it
+        HashSet<String> uniqueWords = new HashSet<>();
+        uniqueWords.addAll(allWords);
+        System.out.println(uniqueWords);
+
+
+
+
     }
+
+    //Print the total number of words found
+    public Integer wordCount(ArrayList<String> lines) {
+        int sum = 1;
+        for(String line : lines) {
+            String[] words = line.split("\\W+");
+            sum = sum + words.length;
+        }
+        return sum;
+    }
+
+    //Generate the count for each word and store the count for each word as HashMap. The number of occurrences for each word
+    public HashMap<String, Integer> countsOfEachWord(ArrayList<String> allWords) {
+        for(String word: allWords) {
+            if (wordsHashMap.containsKey(word)) {
+                wordsHashMap.put(word, wordsHashMap.get(word) + 1);
+            } else {
+                wordsHashMap.put(word, 1);
+            }
+        }
+        return wordsHashMap;
+    }
+
+
+
+
+
+
+
+
 }
